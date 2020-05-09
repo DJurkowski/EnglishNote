7e77b6337b56bac421a40ec39b8e104163907ea8
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-05-09 21:34:59 +0200

    Continued adding validator and working mongoDB user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require("express-validator/check");
+
+const User = require('../../models/User');
 
 // @route   POST api/users
 // @desc    Register user
@@ -9,13 +13,49 @@ router.post('/', [
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
-],(req, res) => {
+],
+async(req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
+
+    const {name, email, password} = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if(user){
+            res.status(400).json({ errors: [{msg: 'User already exists'}]});
+        }
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        })
+
+        user = new User({
+            name, email, avatar, password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+
+
+        res.send('User registered');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+
+
+    // Rturn jsonwebtoken
     
-    res.send('User route');
 
 });
 
