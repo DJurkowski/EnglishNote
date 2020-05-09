097e42189b9f0da7ad2dac6a1236e1fed0bb0d73
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-05-09 22:03:14 +0200

    Added jwt which gives us token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require("express-validator/check");
 
 const User = require('../../models/User');
@@ -45,9 +47,24 @@ async(req, res) => {
 
         await user.save();
 
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+        jwt.sign(
+            payload, 
+            config.get('jwtSecret'),
+            {expiresIn: 360000},  //zmienic na 3600 na godzine 
+            (err, token) => {
+                if(err) throw err;
+                res.json({token});
+            }
+        );
+        // res.send('User registered');
 
 
-        res.send('User registered');
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
