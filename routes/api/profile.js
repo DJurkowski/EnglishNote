1333d8dbead6ff638e238f7e2be8c9768bb67b6b
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-05-11 00:21:13 +0200

    Added http request which deleting profile, user & in future folders

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -134,4 +134,26 @@ router.get('/user/:user_id', async (req, res) => {
     }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user & folders
+// @access  Public
+router.delete('/', auth, async (req, res) => {
+    try {
+
+        // @todo - remove users folders
+
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted '});
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
