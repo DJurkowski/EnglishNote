b72b1b4e0489cd5bf60bf44ad3cac143d21aada7
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-05-23 00:05:40 +0200

    Added current links and close after clicks

--- client/src/components/header/collapsemenu/Collapsemenu.js
+++ client/src/components/header/collapsemenu/Collapsemenu.js
@@ -1,29 +1,56 @@
 import React from 'react';
-// import {NavLink} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import styles from './Collapsemenu.module.scss';
+import PropTypes from 'prop-types';
+import { logout } from '../../../actions/auth';
+import { connect } from 'react-redux';
 import useWindowDimensions from '../../../hooks/useWindowDimensions'
 
 
-const CollapseMenu = (props) => {
+const CollapseMenu = ({ navbarState, handleNavbar, auth: { isAuthenticated, loading }, logout}) => {
    
     const {width} = useWindowDimensions();
+
+    const authLinks = (
+        <>
+            <li><Link onClick={()=> handleNavbar(false)} className={styles.navbarItem} to='/posts'>posts</Link></li>
+            <li><Link onClick={()=> handleNavbar(false)} className={styles.navbarItem} to='/myfolders'>my folders</Link></li>
+            <li><Link onClick={()=> handleNavbar(false)} className={styles.navbarItem} to='/allfolders'>all folders</Link></li>
+            <li><Link onClick={()=> handleNavbar(false)} className={styles.navbarItem} to='/create-folder'>add folder</Link></li>
+            <li><Link onClick={()=> handleNavbar(false)} className={styles.navbarItem} to='/dashboard'>dashboard</Link></li>
+            <li><Link onClick={()=> handleNavbar(false)} onClick={logout} className={styles.navbarItem} to='/'>logout</Link></li>
+        </>
+    );
+
+    const questLinks = (
+        <>
+            <li><Link onClick={()=> handleNavbar(false)} className={styles.navbarItem} to='/register'>register</Link></li>
+            <li><Link onClick={()=> handleNavbar(false)} className={styles.navbarItem} to='/login'>login</Link></li>    
+        </>
+    );
     
-    if(props.navbarState === true && width <= 769){
+    if(navbarState === true && width <= 769) {
         return (
             
             <div className={styles.wrapper}>
                 <ul className={styles.navLinks}>
-                    <li><div className={styles.navbarItem} >about</div></li>
-                    <li><div className={styles.navbarItem} >folders</div></li>
-                    <li><div className={styles.navbarItem} >login</div></li>
-                    <li><div className={styles.navbarItem} >sing up</div></li>
+                    <li><Link onClick={()=> handleNavbar(false)} className={styles.navbarItem} to='/profiles'>profiles</Link></li>
+                    {(!loading && isAuthenticated) ? authLinks : questLinks}
                 </ul>
             </div>
         );
     } else {          
         return null;
     }
-
 };
 
-export default CollapseMenu;
+CollapseMenu.propTypes = {
+    auth: PropTypes.object.isRequired,
+    logout: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps, { logout })(CollapseMenu);
