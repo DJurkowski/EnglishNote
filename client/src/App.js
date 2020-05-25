ee632c65755c77e451a290d6657261120f2fd55b
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-05-25 04:15:04 +0200

    RWD. Responsive for folders display

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Header from './components/header/Header';
 import Routes from './components/routing/Routes';
 // Redux
