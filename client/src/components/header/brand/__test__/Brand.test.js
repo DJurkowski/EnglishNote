c4966cb39508275bf604faf157b64deba0894009
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-06-04 19:55:01 +0200

    Refactor. Test function moved to external file

--- client/src/components/header/brand/__test__/Brand.test.js
+++ client/src/components/header/brand/__test__/Brand.test.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Brand from '../Brand';
 
+import { findByTestAtrr } from '../../../../utils/testFunctions';
+
 const setUp = ( props={} ) => {
     return shallow(<Brand {...props} />);
-};
-
-const findByTestAttr = ( component, attr ) => {
-    return component.find(`[data-test='${attr}']`);
 };
 
 describe('Brand component', () => {
@@ -18,12 +16,12 @@ describe('Brand component', () => {
     });
 
     test('Should render a logo', () => {
-        const wrapper = findByTestAttr(component, 'logo');
+        const wrapper = findByTestAtrr(component, 'logo');
         expect(wrapper.length).toBe(1);
     });
     
     test('Should render a mini logo', () => {
-        const wrapper = findByTestAttr(component, 'miniLogo');
+        const wrapper = findByTestAtrr(component, 'miniLogo');
         expect(wrapper.length).toBe(1);
     });
 
