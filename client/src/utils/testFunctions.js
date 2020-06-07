675638f896bcbbfe2142c1ab6cade5bed6fbbe0f
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-06-07 17:27:37 +0200

    Added function which create test store

--- client/src/utils/testFunctions.js
+++ client/src/utils/testFunctions.js
@@ -1,4 +1,7 @@
 import checkPropTypes from 'check-prop-types';
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from '../reducers/index';
+import { middleware } from '../store';
 
 export const findByTestAtrr = ( component, attr ) => {
     const wrapper = component.find(`[data-test='${attr}']`);
@@ -8,4 +11,9 @@ export const findByTestAtrr = ( component, attr ) => {
 export const checkProps = ( component, expectedProps ) => {
     const propErr = checkPropTypes(component.propTypes, expectedProps, 'props', component.name);
     return propErr;
+};
+
+export const testStore = (initialState) => {
+    const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
 };
