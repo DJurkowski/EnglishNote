a9c8917f0fbde981c4f901e1765e9f349913e741
Domino <jurkowski.dominik.andrzej@gmail.com>
2020-06-07 17:27:01 +0200

    Added tests for collapsemenu component

--- client/src/components/header/collapsemenu/__test__/Collapsemenu.test.js
+++ client/src/components/header/collapsemenu/__test__/Collapsemenu.test.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux';
 import Collapsemenu from '../Collapsemenu';
 
-import { findByTestAtrr, checkProps } from '../../../../utils/testFunctions';
+import { findByTestAtrr, checkProps, testStore } from '../../../../utils/testFunctions';
+
+// const setUp = (initialState={}, props={}) => {
+//     const store = testStore(initialState);
+//     const wrapper = mount(<Provider store={store}><Collapsemenu {...props}/></Provider>);
+//     console.log(wrapper.debug());
+//     return wrapper;
+// };
 
 describe('Collapsemenu component', ()=> {
+
+    // describe('Render component elements', ()=> {
+    //     test('Should return null', ()=> {
+    //         const initialProps = {
+    //             auth: {
+    //                 isAuthenticated: true,
+    //                 loading: false
+    //             },
+    //             logout(){},
+    //             navbarState: false,
+    //             handleNavbar(){}
+    //         }
+    //         const component = setUp({},initialProps);
+    //         expect(component).toBeEmpty();
+    //     });
+
+    // });
 
     describe('Checking PropTypes', ()=> {
 
